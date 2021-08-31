0f479d4c05f7fd57074141b1302d3ea6aafb7637
Mevlut <36679013+mevlut36@users.noreply.github.com>
2021-08-31 04:18:20 +0200

    Update main.java

--- main.java
+++ main.java
@@ -8,28 +8,30 @@ public class Main {
 		System.out.println("! Mev Fight !");
 		//
 		String name = "Mevlut";
-		int life = 20;
+		int life = 100;
 		int power = 150;
-		int defense = 50;
 		
 		int tower_life = 1000;
 		int tower_defense = 50;
 		
 		if(name != null) {
 			int attack = tower_life - power - tower_defense;
-			System.out.println(attack);
+			System.out.println(attack + "800HP - Tower");
 		}
 		
-		//TimeUnit.SECONDS.sleep(3);
-		
-		if(name != null) {
-			int attack = life - tower_defense;
-			System.out.println(attack);
+		while(true) {
+			int life_fight = life - tower_defense;
+			System.out.println(life_fight);
+			TimeUnit.SECONDS.sleep(2);
 			
-			if(attack < 0) {
-				System.out.println("You lose");
+			System.out.println("GG " + name + ", You survived");
+			
+			if(life_fight < 0) {
+				System.out.println(name + " You die");
+				break;
 			}
 		}
+		
 		
 		//SUITE AVEC DES FONCTIONS...
 		
